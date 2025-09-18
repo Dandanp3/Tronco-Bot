df578f1be67f84fe16c2e0fafcd1b809372de779
Daniel <daniel.pyoliverp@gmail.com>
2025-09-18 07:57:03 -0300

    Update main.py

--- Tronco/main.py
+++ Tronco/main.py
@@ -3,26 +3,20 @@ from discord.ext import commands
 from teste_realismo import add, solicitar, corrigir, info  
 
 intents = discord.Intents.default()
-intents.message_content = True  # Necessário se for usar on_message
+intents.message_content = True 
 
 class Tronco(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix="!", intents=intents)
 
     async def setup_hook(self):
-        print("Setup hook chamado - adicione extensões aqui se quiser")
-
         # Comando slash de adicionar questão
         await add.setup(self)
-
-        # Comando !solicitar
+        #
+        
         solicitar.setup(self)
-
-        # Comando !corrigir
         corrigir.setup(self)
         info.setup(self)
-
-        # Sincroniza comandos slash (caso tenha)
         await self.tree.sync()
 
     async def on_ready(self):
